0e4cbc6f18e5d9728026f8478f93c0620b2b1954
felruizrojas <169380595+felruizrojas@users.noreply.github.com>
2024-06-25 11:49:28 -0400

    añadido por fel

---
+++ funciones.py
@@ -0,0 +1,24 @@
+#comprar pasajes
+
+
+'''
+Comprar pasajes: El sistema debe solicitar al cliente la cantidad de pasajes a comprar.
+
+Una vez ingresada la cantidad dentro de los rangos válidos, el sistema desplegará en pantalla las ubicaciones disponibles donde aparecerán con X las ubicaciones ya vendidas y las disponibles con una O.
+
+El cliente seleccionará de una en una las ubicaciones, no pudiendo adquirir las ya vendidas mostrando en pantalla el mensaje adecuado.
+
+Los precios de los asientos son los siguientes:
+• Asiento común, $60.000 pesos. (Celestes)
+• Espacio adicional para piernas, $80.000 pesos. (Rosados)
+• Preferente, $100.000 pesos
+
+Se debe registrar el rut, nombre, apellido de la persona que ocupará cada asiento. El RUT se debe registrar en formato numérico y sin guion ni puntos (por ejemplo: si el RUT de la persona es 12.345.678-9, se debe
+registrar como 12345678 sin dígito verificador).
+
+Posteriormente debe mostrar un mensaje que indique que la operación se ha realizado correctamente.
+'''
+
+
+comprar=input("Ingrese cantidad de pasajes a comprar: ") #198
+
